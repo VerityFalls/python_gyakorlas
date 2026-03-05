1c1eb1a005b87381ca6580f0f15c1f9978319cb5
Istvan Vincze <pilu110@gmail.com>
2026-03-05 17:32:15 +0100

    gyakorlas2

---
+++ main2.py
@@ -0,0 +1,29 @@
+def feldolgozas(kerdes):
+    print("feldolgozas alatt: " + kerdes)
+
+    szamjegyek = dict()
+    for betu in kerdes:
+        print(betu)
+        if betu in ("0" "1" "2" "3" "4" "5" "6" "7" "8" "9"):
+            if betu in szamjegyek:
+            szamjegyek[betu] += 1
+
+
+
+
+   # return kerdes[::-1]
+
+while True:
+    kerdes = input("kerdes: ")
+    if kerdes == "exit":
+        print("bye")
+        break
+    print("Ezt kerdezte: " + kerdes)
+
+    valasz = feldolgozas(kerdes)
+
+    print("Valasz: " + str(valasz))
+
+print ("VEGE.")
+
+
